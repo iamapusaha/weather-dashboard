9704b4d24ff64801761b0d1f32ed7d904c729c56
iamapusaha <apusaha392@gmail.com>
2025-12-02 22:18:17 +0600

    added page.jsx file

---
+++ src/Page.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import WeatherBoard from "./components/Weather/WeatherBoard";
+export default function Page() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  );
+}
